51868b82d9c65d434cf99c7b004d3b8baa6e12a4
MaYiFei1995 <lnfxmyf@126.com>
2022-11-09 15:07:56 +0800

    support jd

---
+++ app/src/main/java/com/mai/fktb/FakeJDActivity.kt
@@ -0,0 +1,13 @@
+package com.mai.fktb
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class FakeJDActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        finish()
+    }
+
+}
